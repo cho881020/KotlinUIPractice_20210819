bf285a500d29ca37cebcc64284248cc9676a33dd
조경진 <cho881020@gmail.com>
2021-08-19 12:10:45 +0900

    입력한 내용을 받아와서, 토스트로 띄우기

--- app/src/main/java/com/neppplus/kotlinuipractice_20210819/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinuipractice_20210819/MainActivity.kt
@@ -2,10 +2,24 @@ package com.neppplus.kotlinuipractice_20210819
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toastBtn.setOnClickListener {
+
+//            입력한 메세지가 뭔지? 변수에 저장.
+//            messageEdt의 text속성에 담겨있는 값을 => inputMessage 변수에 담자.
+            val inputMessage = messageEdt.text.toString()
+
+//            토스트로 inputMessage를 띄워주자.
+            Toast.makeText(this, inputMessage, Toast.LENGTH_SHORT).show()
+
+        }
+
     }
 }
